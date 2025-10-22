d0f0c9b10d2eebbbafa2159ce7069948ea238339
Natalia Garcia <natgarcia@csu.fullerton.edu>
2025-10-22 02:00:34 +0000

    add input format & comments

--- algo2.py
+++ algo2.py
@@ -1,7 +1,7 @@
 # Project 2
 # Algorithm 2 - String Run Encoding
 # ===================================
-# Names:
+# Member Names:
 # - Michael Bui
 # - Natalia Garcia
 # ===================================
@@ -25,28 +25,41 @@
 
 """Function that return a string with the number of similar characters
    and the similar character."""
-def StrRunEncode(string):
-    curr = string[0]
+def StrRunEncode(str_input):
+    curr = str_input[0] # initialized to first character of input
     count = 0
-    new_string = ""
-
-    if not string:
+    new_string = "" # empty string to hold new string
+    
+    # handles error if input is not string
+    if not str_input: 
         return ""
     
-    if string.islower():
-        for char in string:
+    if str_input.islower(): # checks if input is lowercase
+        # iterates through each character in the string
+        for char in str_input:
             if curr == char:
                 count += 1
             else: 
+                # executes when count is greater than 1
                 if count > 1:
+                    # converts count to string & adds to new string
                     new_string += str(count)
-                new_string += curr
+                # adds current character to new string    
+                new_string += curr 
                 
-                count = 1 
-                curr = char
+                count = 1 # resets count
+                curr = char # updates current character
 
         if count > 0:
-            intostr = str(count)
-            new_string += f"{intostr}{curr}"
-            count = 0      
+            intostr = str(count) # convert to string
+            # add count & current character to new string
+            new_string += f"{intostr}{curr}" 
+            count = 0 # resets count   
     return new_string
+
+# File to read input from
+SAMPLE_INPUT = "input.txt"
+
+with open(SAMPLE_INPUT, "r") as file:
+    lines = file.read()
+    print(StrRunEncode(lines))
